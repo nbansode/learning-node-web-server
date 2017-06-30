77223680ed0dbe07cbee39f5fdf2fe2e32513c2e
neel bansode <neelb18@gmail.com>
2017-06-30 19:14:25 +0530

    Added Project page

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express');
-const hbs=require('hbs');
-const fs= require('fs');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
-const port= process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-hbs.registerPartials(__dirname+'/views/partials');
-app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 
-hbs.registerHelper('getCurrentYear',()=>{
+hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt',(text)=>{
+hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
-app.use((req,res,next)=>{
-  var now= new Date().toString();
-  var log= `${now}:${req.method} ${req.url}`;
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log',log+'\n',(err)=>{
+  fs.appendFile('server.log', log + '\n', (err) => {
     if (err) {
       console.log('Unable to append to server.log');
     }
@@ -34,29 +34,34 @@ app.use((req,res,next)=>{
 //   res.render('maintenance.hbs');
 // });
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.render('home.hbs',{
-    pageTitle:'Home Page',
-    welcomeMessage:'Welcome to my website',
-    currentYear:new Date().getFullYear()
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
   })
 
 });
 
-app.get('/about',(req,res)=>{
-  res.render('about.hbs',{
-    pageTitle:'About Page',
-    currentYear:new Date().getFullYear()
-  })});
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  })
+});
 
-app.get('/bad',(req,res)=>{
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page',
+  })
+});
+
+app.get('/bad', (req, res) => {
   res.send({
-    errorMessage:'Unable to handle request'
+    errorMessage: 'Unable to handle request'
   });
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
